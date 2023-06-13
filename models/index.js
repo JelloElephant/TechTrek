fc076285a6604c536472a17e5b2ff6bf4e11c049
lauren haas <laurenraehaas@gmail.com>
2023-06-12 20:07:26 -0500

    updating index.js

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const User = require('./User');
 
 module.exports = { User };
 
-// connecting trips and user by their foreign key
+// connecting trips to user with their foreign key
 Trips.belongsTo(User, {
     foreignKey: 'user_id'
 })
