da6d477ba59ffa930884ac6b16fa90caca5bb07a
lauren haas <laurenraehaas@gmail.com>
2023-06-12 19:06:27 -0500

    adding trips.js

---
+++ models/trips.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Trips extends Model {}
+
+Trips.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        place: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        description: {
+            type: DataType.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'trips',
+    }
+);
+
+module.exports = Trips;
