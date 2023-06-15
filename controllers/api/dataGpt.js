7eab3c734db2bbee63c490f67637d6f0be3faba1
jimh <james.l.heinecke@gmail.com>
2023-06-14 21:22:18 -0500

    chatgpt updates

--- controllers/api/dataGpt.js
+++ controllers/api/dataGpt.js
@@ -1,15 +1,16 @@
 const router = require('express').Router();
 
+/* const { query } = require('express'); */
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
   apiKey: process.env.API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-const query = '10 things to do in minneapolis on June 15'
+/* const query = '10 things to do in minneapolis on June 15' */
 
 router.post('/', async (req, res) => {
   try {
-const payload = {'role':'user','content':query}
+const payload = {'role':'user','content':req.body.query}
 const completion = await openai.createChatCompletion({
   model: "gpt-3.5-turbo",
   messages: [payload],
@@ -22,6 +23,7 @@ res.send(output);
   } catch (err) {
     res.status(400).json(err);
   }
+/*   return output; */
 });
 
 module.exports = router;
